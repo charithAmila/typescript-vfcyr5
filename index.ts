9be1afa4dccb956166ea937d1a1b7eca8ab19c1a
charithAmila <dkcadissanayaka@gmail.com>
2022-07-25 20:49:27 +0530

    Test commit

--- index.ts
+++ index.ts
@@ -1,12 +1,6 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1> <canvas id="canvas"></canvas>`;
-
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 150, 100);
+const appDiv: HTMLElement = document.getElementById("app");
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
