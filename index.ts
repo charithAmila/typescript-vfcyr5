2f7e28d8a0cd2a532913f60eaf124cf3d211cdaa
charithAmila <dkcadissanayaka@gmail.com>
2022-07-26 23:24:38 +0530

    Add changes

--- index.ts
+++ index.ts
@@ -1,79 +1,86 @@
 // Import stylesheets
-import './style.css';
+import { Point } from "./PointInterface";
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<canvas id="canvas"></canvas>`;
 
-var canvas = document.getElementById('canvas') as HTMLCanvasElement | null;
+var canvas = document.getElementById("canvas") as HTMLCanvasElement | null;
 
 canvas.width = document.body.clientWidth;
 canvas.height = document.body.clientHeight;
 
-var ctx = canvas.getContext('2d');
+var ctx = canvas.getContext("2d");
 
-const points = [
+const poly: Point[] = [
   { x: 10, y: 10 },
   { x: 200, y: 10 },
   { x: 200, y: 200 },
   { x: 10, y: 200 },
 ];
 
-var coordinates = [];
-
-for (let i = 0; i < points.length; i++) {
-  const data = getAllCoordinates(points[i], points[i + 1] ?? points[0]);
-  coordinates.push(...data);
-}
-
-var bullet = [points[0].x, points[0].y];
+var bullet = { x: poly[0].x, y: poly[0].y };
 
 const draw = function () {
-  ctx.fillStyle = '#FFFF';
+  ctx.fillStyle = "#FFFF";
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillRect(bullet[0] - 2, bullet[1] - 2, 4, 4);
+  ctx.fillRect(bullet.x - 2, bullet.y - 2, 4, 4);
 
-  ctx.globalCompositeOperation = 'destination-over';
-  ctx.fillStyle = 'rgb(63, 63, 63)';
-  ctx.strokeStyle = 'rgb(173, 172, 172)';
+  ctx.globalCompositeOperation = "destination-over";
+  ctx.fillStyle = "rgb(63, 63, 63)";
+  ctx.strokeStyle = "rgb(173, 172, 172)";
 
   ctx.beginPath();
-  for (let index = 0; index < points.length; index++) {
+  for (let index = 0; index < poly.length; index++) {
     if (index === 0) {
-      ctx.moveTo(points[index].x, points[index].y);
+      ctx.moveTo(poly[index].x, poly[index].y);
     }
-    ctx.lineTo(points[index].x, points[index].y);
+    ctx.lineTo(poly[index].x, poly[index].y);
   }
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
 };
 
-canvas.addEventListener('mousemove', function (e) {
-  var cRect = canvas.getBoundingClientRect();
-  var canvasX = Math.round(e.clientX - cRect.left);
-  var canvasY = Math.round(e.clientY - cRect.top);
-  const closestPint = coordinates
-    .map((ax) => {
-      let y = canvasX - ax[0];
-      let x = canvasY - ax[1];
+function closestPointInPolygon(poly: Point[], pos: Point): Point {
+  var coordinates = [];
+
+  for (let i = 0; i < poly.length; i++) {
+    const data = getAllCoordinates(poly[i], poly[i + 1] ?? poly[0]);
+    coordinates.push(...data);
+  }
+
+  const { x, y } = coordinates
+    .map((coordinate) => {
+      let y = pos.x - coordinate[0];
+      let x = pos.y - coordinate[1];
 
       return {
         distance: Math.sqrt(x * x + y * y),
-        x: ax[0],
-        y: ax[1],
+        x: coordinate[0],
+        y: coordinate[1],
       };
     })
     .sort(function (a, b) {
       return a.distance - b.distance;
-    });
-  bullet = [closestPint[0].x, closestPint[0].y];
+    })[0];
+
+  return { x, y };
+}
+
+canvas.addEventListener("mousemove", function (e) {
+  var cRect = canvas.getBoundingClientRect();
+  var canvasX = Math.round(e.clientX - cRect.left);
+  var canvasY = Math.round(e.clientY - cRect.top);
+  const closestPint = closestPointInPolygon(poly, { x: canvasX, y: canvasY });
+  bullet = closestPint;
   draw();
 });
 
-function getAllCoordinates(A, B) {
-  function slope(a, b) {
+function getAllCoordinates(A: Point, B: Point) {
+  function slope(a: Point, b: Point) {
     if (a.x == b.x) {
       return null;
     }
@@ -81,7 +88,7 @@ function getAllCoordinates(A, B) {
     return (b.y - a.y) / (b.x - a.x);
   }
 
-  function intercept(point, slope) {
+  function intercept(point: Point, slope: number | null) {
     if (slope === null) {
       return point.x;
     }
@@ -114,7 +121,6 @@ function getAllCoordinates(A, B) {
       coordinates.push([x, y]);
     }
   }
-
   return coordinates;
 }
 
